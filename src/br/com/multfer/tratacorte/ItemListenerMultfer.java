19deab3810294e75049931c8f8604bc10364974a
Eliezer Moraes Silva <eliezer.moraes@outlook.com>
2025-12-13 10:33:20 -0300

    :sparkles: refactor: Corrigido lógica de cálculo do valor de desconto vlrDesc

--- src/br/com/multfer/tratacorte/ItemListenerMultfer.java
+++ src/br/com/multfer/tratacorte/ItemListenerMultfer.java
@@ -111,12 +111,13 @@ public class ItemListenerMultfer implements EventoProgramavelJava {
                 JapeWrapper pedidoDAO = JapeFactory.dao("ItemNota");
                 DynamicVO itemPedidoVO = pedidoDAO.findByPK(new Object[]{ itemVO.asBigDecimal("AD_NUNOTAPED"), itemVO.asBigDecimal("AD_SEQUENCIAPED") });
 
-                BigDecimal vlrUnit = itemVO.asBigDecimal("VLRUNIT");
-                BigDecimal qtdNeg = itemVO.asBigDecimal("QTDNEG");
-                BigDecimal percDesc = itemPedidoVO.asBigDecimal("PERCDESC");
+                BigDecimal vlrUnit = itemVO.asBigDecimal("VLRUNIT"); // 58,04
+                BigDecimal qtdNeg = itemVO.asBigDecimal("QTDNEG"); // 25
+                //BigDecimal percDesc = itemPedidoVO.asBigDecimal("PERCDESC"); // 17
 
                 BigDecimal vlrTot = vlrUnit.multiply(qtdNeg).setScale(2, RoundingMode.HALF_UP);
-                BigDecimal vlrDesc = vlrTot.multiply(percDesc).divide(new BigDecimal("100"), 2, RoundingMode.HALF_UP);
+                //BigDecimal vlrDesc = vlrTot.multiply(percDesc).divide(new BigDecimal("100"), 2, RoundingMode.HALF_UP); // 246,67
+                BigDecimal vlrDesc = itemVO.asBigDecimal("VLRDESC");
 
                 itemVO.setProperty("VLRDESC", vlrDesc);
                 itemVO.setProperty("VLRTOT", vlrTot);
